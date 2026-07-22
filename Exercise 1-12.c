2fead81878efebfe147a54dd19e805fec2788848
mubasharameen485-cloud <mubasharameen485@gmail.com>
2026-07-22 09:25:06 +0500

    exer 11,12

---
+++ Exercise 1-12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF) {
+
+        if (c == ' ' || c == '\n' || c == '\t')
+            printf("\n");
+        else
+            printf("%c", c);
+
+    }
+
+    return 0;
+}
